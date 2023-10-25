e5ace099f84e264cd7fb23037893ff1afdb6f82f
seanmalley <sean.malley1@protonmail.com>
2023-10-24 21:38:21 -0400

    On Q4, need to combine the position group. have 4 infields, 2 battery

--- scripts/baseball.sql
+++ scripts/baseball.sql
@@ -4,16 +4,19 @@ FROM homegames
 --Answer: 1871 - 2016
 
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
-SELECT playerid,height, namefirst, namelast, teams.teamid, bats, throws, g_all, birthyear,deathyear,debut,finalgame
+
+SELECT playerid,height, namefirst, namelast, appearances.teamid, bats, throws, g_all, birthyear,deathyear,debut,finalgame, teams.name
 FROM people
-LEFT JOIN managershalf
-USING (playerid)
-LEFT JOIN teams
-USING(teamid)
 LEFT JOIN appearances
 USING(playerid)
+LEFT JOIN teams
+USING(teamid)
 ORDER BY height ASC
 LIMIT 1
+
+
+
+
 --Answer: Eddie Gaedel, 43. No team that I can find, One game.
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each playerâ€™s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 SELECT namefirst, namelast, schoolid, SUM(salaries.salary) as sumsal, salaries.yearid,salary
@@ -31,6 +34,19 @@ ORDER BY sumsal DESC
 
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+SELECT
+	CASE 
+		WHEN pos = 'OF' THEN 'Outfield' 
+		WHEN pos IN ('SS', '1B', '2B', '3B') THEN 'Infield'
+		WHEN pos IN ('P', 'C') THEN 'Battery'
+		END AS position,
+		SUM(po) AS Putouts
+FROM fielding		
+INNER JOIN people
+USING (playerid)
+WHERE fielding.yearid = '2016'
+GROUP BY  pos
+ORDER BY Putouts DESC;
    
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
    
