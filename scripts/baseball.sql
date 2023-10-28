89ea3fcc50e86c4032a85a3bd02007e4b0c673c7
seanmalley <sean.malley1@protonmail.com>
2023-10-28 11:00:42 -0400

    working through q8

--- scripts/baseball.sql
+++ scripts/baseball.sql
@@ -107,7 +107,15 @@ GROUP BY yearid
 
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
---ATL PBA OAK CLE MI
+--ATL PBA OAK CLE MI max , trop, 15k , oakland, 18k. progressive 19k, marlins 21k, chi 
+
+SELECT * from homegames where games >= 10
+
+SELECT park, (attendance/games) as avg_attendence
+from homegames 
+where games >= 10 AND year = 2016
+ORDER BY park
+
 
 
 
