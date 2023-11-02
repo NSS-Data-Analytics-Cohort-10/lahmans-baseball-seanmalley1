c86e0792e0c9d2a34d89539c418eae925e1ef216
seanmalley <sean.malley1@protonmail.com>
2023-11-02 19:43:52 -0400

    starting on 11

--- scripts/baseball.sql
+++ scripts/baseball.sql
@@ -201,7 +201,8 @@ HAVING
 -- ANSWER: Davey Johnson, managing Baltimore Orioles, Cincinnati Reds, LA Dodgers, New York Mets, Washington Nationals, Washington Senators, and Jim Leyland, managing Colorado Rockies, Detriot Tigers, Florida MArlins, Pittsburg Pirates
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
--- should be around 
+
+--=-=-=-==-=-=-=-=-=----------=====-----------------------
 
 SELECT 
     p.nameFirst,
